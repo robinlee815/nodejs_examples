a7777c374c7802578f22253b644928366ec016bc
robinlee815 <yeoreum.lee@codecrain.com>
2019-06-09 20:28:29 +0900

    2019-06-09

---
+++ exterior.module/jade.basic.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var fs = require('fs');
+var jade = require('jade');
+
+http.createServer(function (request, response) {
+    fs.readFile('jadePage.jade', 'utf8', function(error, data) {
+        var fn = jade.compile(data);
+
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(fn());
+    });
+}).listen(52273, function() {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
